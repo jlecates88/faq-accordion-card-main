3f0ec953a2ed8a684c1ab7b102d7fe97942cb59b
Aman=sgz <67084595+Aman-sgz@users.noreply.github.com>
2020-10-25 23:55:20 -0300

    in progress

--- js/app.js
+++ js/app.js
@@ -1,21 +1,10 @@
-var acc = document.getElementsByClassName("accordion");
-var panel = document.getElementsByClassName('panel');
+const accordion = document.getElementById('accordion');
+const accordionTexts = [...document.querySelectorAll('.accordion__text')];
 
-for (var i = 0; i < acc.length; i++) {
-    acc[i].onclick = function() {
-    	var setClasses = !this.classList.contains('active');
-        setClass(acc, 'active', 'remove');
-        setClass(panel, 'show', 'remove');
-        
-       	if (setClasses) {
-            this.classList.toggle("active");
-            this.nextElementSibling.classList.toggle("show");
-        }
+accordion.addEventListener('click', e => {
+    const targetClass = e.target.classList;
+    if(targetClass.contains('accordion__title')) {
+        e.target.nextElementSibling.classList.toggle('accordion__text--active');
+        e.target.classList.toggle('accordion__title--active')
     }
-}
-
-function setClass(els, className, fnName) {
-    for (var i = 0; i < els.length; i++) {
-        els[i].classList[fnName](className);
-    }
-}
+})
