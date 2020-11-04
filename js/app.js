efda5ec444e4e4e2bec1fcf70ce77f5cd71e1aca
Aman=sgz <67084595+Aman-sgz@users.noreply.github.com>
2020-11-03 22:59:08 -0300

    Working on desktop design

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
-const accordion = document.getElementById('accordion');
-const accordionTexts = [...document.querySelectorAll('.accordion__text')];
+const accordion = document.getElementById('accordion-container');
+const accordionAnswer = [...document.querySelectorAll('.accordion__answer')];
 
 accordion.addEventListener('click', e => {
     const targetClass = e.target.classList;
-    if(targetClass.contains('accordion__title')) {
-        e.target.nextElementSibling.classList.toggle('accordion__text--active');
-        e.target.classList.toggle('accordion__title--active')
+    if(targetClass.contains('accordion__question')) {
+        e.target.nextElementSibling.classList.toggle('accordion__answer--active');
+        e.target.classList.toggle('accordion__question--active')
     }
 })
