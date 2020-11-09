c8aa0c2a1f331c20b6fc8d2d9056c33d984e36c9
Aman=sgz <67084595+Aman-sgz@users.noreply.github.com>
2020-11-09 18:46:10 -0300

    updated

--- js/app.js
+++ js/app.js
@@ -4,7 +4,24 @@ const accordionAnswer = [...document.querySelectorAll('.accordion__answer')];
 accordion.addEventListener('click', e => {
     const targetClass = e.target.classList;
     if(targetClass.contains('accordion__question')) {
-        e.target.nextElementSibling.classList.toggle('accordion__answer--active');
-        e.target.classList.toggle('accordion__question--active')
+      e.target.classList.toggle('accordion__question--active') //roto flecha y negrita al h2
+     // e.target.nextElementSibling.classList.toggle('accordion__answer--active');
     }
 })
+
+/*  (function() {
+    const headings = document.querySelectorAll('h2');
+    
+    Array.prototype.forEach.call(headings, h => {
+      let btn = h.querySelector('button');
+      let target = h.nextElementSibling;
+      
+      btn.onclick = () => {
+        let expanded = btn.getAttribute('aria-expanded') === 'true';
+        
+        btn.setAttribute('aria-expanded', !expanded);
+        target.hidden = expanded;  
+      }
+    });
+  })()
+ */
