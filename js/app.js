0a1f6d9c00cfbb1e848ecbf1b0aa2a5ca8279775
Aman=sgz <67084595+Aman-sgz@users.noreply.github.com>
2020-11-10 08:55:51 -0300

    js accordion

--- js/app.js
+++ js/app.js
@@ -1,27 +1,11 @@
 const accordion = document.getElementById('accordion-container');
-const accordionAnswer = [...document.querySelectorAll('.accordion__answer')];
+//const accordionAnswer = [...document.querySelectorAll('.accordion__answer')];
 
 accordion.addEventListener('click', e => {
     const targetClass = e.target.classList;
+    //cambio clases css
     if(targetClass.contains('accordion__question')) {
-      e.target.classList.toggle('accordion__question--active') //roto flecha y negrita al h2
-     // e.target.nextElementSibling.classList.toggle('accordion__answer--active');
+      e.target.nextElementSibling.classList.toggle('accordion__answer--active'); //despliego la respuesta 
+      e.target.classList.toggle('accordion__question--active'); //roto flecha y negrita al button
     }
-})
-
-/*  (function() {
-    const headings = document.querySelectorAll('h2');
-    
-    Array.prototype.forEach.call(headings, h => {
-      let btn = h.querySelector('button');
-      let target = h.nextElementSibling;
-      
-      btn.onclick = () => {
-        let expanded = btn.getAttribute('aria-expanded') === 'true';
-        
-        btn.setAttribute('aria-expanded', !expanded);
-        target.hidden = expanded;  
-      }
-    });
-  })()
- */
+});
